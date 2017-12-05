56e0ef104a1b8dbe2ec8b974791fcdbada9defd0
Dylan <Dylan.Nielson@nih.gov>
2017-12-04 22:26:56 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,85 @@
+## Prototype AFNI preprocessing app
+
+### Description
+This is a prototype AFNI bids app implmenting participant level preprocessing with afni_proc.py.
+This pipeline is currently doing temporal alignment, nonlinear registration to standard space,
+ bluring of 4 mm, masking, and scaling for all epis in the input bids dataset using the following 
+ afni proc command:
+```
+afni_proc.py -subj_id {subj_id} \
+ -script proc.bids -scr_overwrite -out_dir {out_dir} \
+-blocks tshift align tlrc volreg blur mask scale \
+-copy_anat {anat_path} -tcat_remove_first_trs 0 \
+-dsets {epi_paths} -align_opts_aea -cost lpc+ZZ -giant_move \
+-tlrc_base MNI152_T1_2009c+tlrc -tlrc_NL_warp \
+-volreg_align_to MIN_OUTLIER \
+-volreg_align_e2a -volreg_tlrc_warp -blur_size 4.0 -bash"""
+```
+
+### Documentation
+Documenation for afni_proc.py is available [here](https://afni.nimh.nih.gov/pub/dist/doc/program_help/afni_proc.py.html).
+
+### How to report errors
+Specific issues with this BIDS App should be reported on its [issues page](https://github.com/nih-fmrif/afni_proc_BIDS_app/issues).
+AFNI issues should be posted to the [AFNI Message Board](https://afni.nimh.nih.gov/afni/community/board/list.php?1)
+
+### Acknowledgements
+Please cite the 1996 paper if you use AFNI:
+ Cox RW (1996) AFNI: Software for analysis and visualization of functional magnetic resonance neuroimages. Comput Biomed Res 29(3):162–173
+
+### Usage
+This App has the following command line arguments:
+
+		usage: run.py [-h]
+		              [--participant_label PARTICIPANT_LABEL [PARTICIPANT_LABEL ...]]
+		              bids_dir output_dir
+
+		Example BIDS App entry point script.
+
+		positional arguments:
+		  bids_dir              The directory with the input dataset formatted
+		                        according to the BIDS standard.
+		  output_dir            The directory where the output files should be stored.
+		                        If you are running a group level analysis, this folder
+		                        should be prepopulated with the results of
+		                        the participant level analysis.
+		  {participant}         Level of the analysis that will be performed. Multiple
+	                            participant level analyses can be run independently
+	                            (in parallel). Only "participant" is currently supported.
+
+		optional arguments:
+		  -h, --help            show this help message and exit
+		  --participant_label PARTICIPANT_LABEL [PARTICIPANT_LABEL ...]
+		                        The label(s) of the participant(s) that should be
+		                        analyzed. The label corresponds to
+		                        sub-<participant_label> from the BIDS spec (so it does
+		                        not include "sub-"). If this parameter is not provided
+		                        all subjects will be analyzed. Multiple participants
+		                        can be specified with a space separated list.
+		  --afni_proc AFNI_PROC
+                                Optional: command string for afni proc. Parameters
+                                that vary by subject should be encapsulated in curly
+                                braces and must all be included {{subj_id}},
+                                {{out_dir}}, {{anat_path}}, or {{epi_paths}}.The first
+                                _T1w for each subject will currently be used as the
+                                anat.All of the _bold will be used as the
+                                functionals.Example:-subj_id {subj_id} -script
+                                proc.bids -scr_overwrite -out_dir {{out_dir}} -blocks
+                                tshift align tlrc volreg blur mask scale -copy_anat
+                                {{anat_path}} -tcat_remove_first_trs 0 -dsets
+                                {{epi_paths}} -align_opts_aea -cost lpc+ZZ -giant_move
+                                -tlrc_base MNI152_T1_2009c+tlrc -tlrc_NL_warp 
+                                -volreg_align_to MIN_OUTLIER 
+                                -volreg_align_e2a -volreg_tlrc_warp -blur_size 4.0 -bash"""
+
+
+To run it in participant level mode (for one participant):
+
+    docker run -i --rm \
+		-v /Users/filo/data/ds005:/bids_dataset:ro \
+		-v /Users/filo/outputs:/outputs \
+		bids/example \
+		/bids_dataset /outputs participant --participant_label 01
+
+### Special considerations
+This is a very early prototype. More functionality is likely coming. Expect breaking changes.
---
+++ run.py
@@ -0,0 +1,364 @@
+#!/usr/bin/env python3
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import argparse
+import os
+import subprocess
+from glob import glob
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+import base64
+import json
+import numpy as np
+
+from io import open  # pylint: disable=W0622
+import jinja2
+
+__version__ = open(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                'version')).read()
+
+class Template(object):
+    """
+    Utility class for generating a config file from a jinja template.
+    https://github.com/oesteban/endofday/blob/f2e79c625d648ef45b08cc1f11fd0bd84342d604/endofday/core/template.py
+    """
+    def __init__(self, template_str):
+        self.template_str = template_str
+        self.env = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(searchpath='/'),
+            trim_blocks=True, lstrip_blocks=True)
+
+    def compile(self, configs):
+        """Generates a string with the replacements"""
+        template = self.env.get_template(self.template_str)
+        return template.render(configs)
+
+    def generate_conf(self, configs, path):
+        """Saves the oucome after replacement on the template to file"""
+        output = self.compile(configs)
+        with open(path, 'w+') as output_file:
+            output_file.write(output)
+
+
+class IndividualTemplate(Template):
+    """Specific template for the individual report"""
+
+    def __init__(self):
+        #super(IndividualTemplate, self).__init__(pkgrf('mriqc', 'data/reports/individual.html'))
+        super(IndividualTemplate, self).__init__('/code/reports/individual.html')
+
+
+class GroupTemplate(Template):
+    """Specific template for the individual report"""
+
+    def __init__(self):
+        #super(GroupTemplate, self).__init__(pkgrf('mriqc', 'data/reports/group.html'))
+        super(GroupTemplate, self).__init__('/code/reports/group.html')
+
+def read_report_snippet(in_file):
+    """Add a snippet into the report"""
+    import os.path as op
+    import re
+    from io import open  # pylint: disable=W0622
+
+    is_svg = (op.splitext(op.basename(in_file))[1] == '.svg')
+
+    with open(in_file) as thisfile:
+        if not is_svg:
+            return thisfile.read()
+
+        svg_tag_line = 0
+        content = thisfile.read().split('\n')
+        corrected = []
+        for i, line in enumerate(content):
+            if "<svg " in line:
+                line = re.sub(' height="[0-9.]+[a-z]*"', '', line)
+                line = re.sub(' width="[0-9.]+[a-z]*"', '', line)
+                if svg_tag_line == 0:
+                    svg_tag_line = i
+            corrected.append(line)
+        return '\n'.join(corrected[svg_tag_line:])
+
+
+def make_montage(prefix, ulay=None, olay=None, cbar='FreeSurfer_Seg_i255',
+                 opacity=4, montx=3, monty=1, blowup=1, delta_slices='-1 -1 -1',
+                 func_range_perc=100):
+
+    if ulay is None and olay is None:
+        raise Exception("overlay and underlay can't both be undefined")
+    elif ulay is None and olay is not None:
+        ulay = olay
+        olay = None
+
+    cmd = '/code/@chauffeur_afni' + \
+    ' -ulay ' + ulay
+    if olay is not None:
+        cmd += ' -olay ' + olay
+        cmd += ' -set_dicom_xyz `3dCM {i}`'.format(i=olay)
+        cmd += ' -cbar ' + cbar + \
+               ' -opacity %d'%opacity 
+    else:
+        cmd += ' -olay_off'
+        cmd += ' -set_dicom_xyz `3dCM {i}`'.format(i=ulay)
+    cmd += ' -prefix ' + prefix + \
+    ' -do_clean' + \
+    ' -delta_slices '+ delta_slices + \
+    ' -montx %d'%montx + \
+    ' -monty %d'%monty + \
+    ' -blowup %d'%blowup + \
+    ' -func_range_perc %f' %func_range_perc + \
+    ' -save_ftype JPEG'
+    return cmd
+
+def make_motion_plot(subj_dir, subj_id):
+    # Read the three files in
+    motion_file = os.path.join(subj_dir,'dfile_rall.1D')
+    motion = pd.read_csv(motion_file, sep='\s*', engine = 'python', names = ['$\Delta$A-P [mm]','$\Delta$L-R [mm]','$\Delta$I-S [mm]','Yaw [$^\circ$]','Pitch [$^\circ$]','Roll [$^\circ$]'])
+    
+    enorm_file = os.path.join(subj_dir,'motion_{subj_id}_enorm.1D'.format(subj_id=subj_id))
+    enorm = pd.read_csv(enorm_file, sep='\s*', engine = 'python', names = ['enorm'])
+
+    outlier_file = os.path.join(subj_dir,'outcount_rall.1D')
+    outliers = pd.read_csv(outlier_file, sep='\s*', engine = 'python', names = ['outliers'])
+    
+    # make a dataframe
+    mot_df = pd.concat([outliers,enorm,motion], axis = 1)
+    
+    # Plot the dataframe
+    axs = mot_df.plot(subplots = True, figsize = (4,5))
+    ldgs = []
+    for ax in axs:
+        box = ax.get_position()
+        ax.legend()
+        ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+        ldgs.append(ax.legend(loc='center left', bbox_to_anchor=(1, 0.5)))
+    plt.tight_layout()
+    # save the figure
+    qc_dir = os.path.join(subj_dir,'qc')
+    img_dir = os.path.join(qc_dir,'img')
+    if not os.path.exists(qc_dir):
+        os.mkdir(qc_dir)
+    if not os.path.exists(img_dir):
+        os.mkdir(img_dir)
+    out_path = os.path.join(img_dir,'motion_plot.svg')
+    
+    plt.savefig(out_path, tight_layout = True, bbox_extra_artists=ldgs, bbox_inches='tight')
+    return out_path
+
+
+def run(command, env={}, shell=False):
+    merged_env = os.environ
+    merged_env.update(env)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT, shell=shell,
+                               env=merged_env)
+    while True:
+        line = process.stdout.readline()
+        line = str(line, 'utf-8')[:-1]
+        print(line)
+        if line == '' and process.poll() is not None:
+            break
+    if process.returncode != 0:
+        raise Exception("Non zero return code: %d"%process.returncode)
+
+
+parser = argparse.ArgumentParser(description='Example BIDS App entrypoint script.')
+parser.add_argument('bids_dir', help='The directory with the input dataset '
+                    'formatted according to the BIDS standard.')
+parser.add_argument('output_dir', help='The directory where the output files '
+                    'should be stored. If you are running group level analysis '
+                    'this folder should be prepopulated with the results of the'
+                    'participant level analysis.')
+parser.add_argument('analysis_level', help='Level of the analysis that will be performed. '
+                    'Multiple participant level analyses can be run independently '
+                    '(in parallel) using the same output_dir.'
+                    'Only "participant" is currently supported.',
+                    choices=['participant', 'group'])
+parser.add_argument('--participant_label', help='The label(s) of the participant(s) that should be analyzed. The label '
+                    'corresponds to sub-<participant_label> from the BIDS spec '
+                    '(so it does not include "sub-"). If this parameter is not '
+                    'provided all subjects should be analyzed. Multiple '
+                    'participants can be specified with a space separated list.',
+                    nargs="+")
+parser.add_argument('--afni_proc', help='Optional: command string for afni proc. '
+                    'Parameters that vary by subject '
+                    'should be encapsulated in curly braces and must all be included '
+                    '{{subj_id}}, {{out_dir}}, {{anat_path}}, or {{epi_paths}}.'
+                    'The first _T1w for each subject will currently be used as the anat.'
+                    'All of the _bold will be used as the functionals.'
+                    'Example:'
+                    '-subj_id {subj_id} '
+                    '-script proc.bids -scr_overwrite -out_dir {{out_dir}} '
+                    '-blocks tshift align tlrc volreg blur mask scale '
+                    '-copy_anat {{anat_path}} -tcat_remove_first_trs 0 '
+                    '-dsets {{epi_paths}} -volreg_align_to MIN_OUTLIER '
+                    '-volreg_align_e2a -volreg_tlrc_warp -blur_size 4.0 -bash')
+parser.add_argument('--report_only', dest='report_only', action='store_true')
+parser.add_argument('-v', '--version', action='version',
+                    version='afni_proc BIDS-App {}'.format(__version__))
+
+
+args = parser.parse_args()
+
+bad_chars = ['`', '|', '&', ';', '>', '<', '$', '?', '(', ')', '\.', ':', '[', ']']
+
+if args.afni_proc is not None:
+    cmd_skeleton = args.afni_proc
+    for bc in bad_chars:
+        if bc in cmd_skeleton:
+            raise Exception("Unsafe character '%s' found in command: %s"%(bc, cmd_skeleton))
+    cmd_skeleton = 'python /opt/afni/afni_proc.py '+ cmd_skeleton
+else:
+    cmd_skeleton = "python /opt/afni/afni_proc.py -subj_id {subj_id} \
+-script proc.bids -scr_overwrite -out_dir {out_dir} \
+-blocks tshift align tlrc volreg blur mask scale \
+-copy_anat {anat_path} -tcat_remove_first_trs 0 \
+-dsets {epi_paths}  -align_opts_aea -cost lpc+ZZ -giant_move \
+-tlrc_base MNI152_T1_2009c+tlrc -tlrc_NL_warp \
+-volreg_align_to MIN_OUTLIER \
+-volreg_align_e2a -volreg_tlrc_warp -blur_size 4.0 -bash"""
+
+run(('bids-validator %s'%args.bids_dir).split(' '))
+
+# Get path for report directory
+reports_dir = os.path.join(args.output_dir,"reports")
+
+
+subjects_to_analyze = []
+# only for a subset of subjects
+if args.participant_label:
+    subjects_to_analyze = args.participant_label
+# for all subjects
+else:
+    subject_dirs = glob(os.path.join(args.bids_dir, "sub-*"))
+    subjects_to_analyze = [subject_dir.split("-")[-1] for subject_dir in subject_dirs]
+
+all_configs = []
+for subject_label in subjects_to_analyze:
+    anat_path = list(glob(os.path.join(args.bids_dir, "sub-%s"%subject_label,
+                                       "anat", "*_T1w.nii*")) + glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"ses-*","anat", "*_T1w.nii*")))[0]
+    epi_paths = ' '.join(list(glob(os.path.join(args.bids_dir, "sub-%s"%subject_label,
+                                                "func", "*bold.nii*")) + glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"ses-*","func", "*bold.nii*"))))
+    subj_out_dir = os.path.join(args.output_dir, "sub-%s"%subject_label)
+    subj_qc_dir = os.path.join(subj_out_dir, 'qc')
+    subj_qc_img_dir = os.path.join(subj_qc_dir, 'img')
+
+    if args.analysis_level == 'participant':
+
+        cmd = cmd_skeleton.format(subj_id=subject_label, out_dir=subj_out_dir,
+                                  anat_path=anat_path, epi_paths=epi_paths)
+        if '{' in cmd:
+            raise Exception("Unsafe character '{' found in command: %s"%cmd.join(' '))
+        cmd = cmd.replace('  ', ' ').split(' ')
+
+        if not args.report_only:
+            print(' '.join(cmd))
+            run(cmd)
+            print("tcsh -xef proc.bids 2>&1 | tee output.proc.bids")
+            run("tcsh -xef proc.bids 2>&1 | tee output.proc.bids", shell=True)
+
+        pbs = glob(os.path.join(subj_out_dir, 'pb*'))
+        if len(pbs) > 0:
+            pb_lod = []
+            for pb in pbs:
+                pbd = {}
+                pbn = pb.split('/')[-1].split('.')
+                pbd['path'] = pb
+                pbd['filename'] = pb.split('/')[-1]
+                pbd['pb'] = int(pbn[0][-2:])
+                pbd['subj'] = pbn[1]
+                pbd['run'] = int(pbn[2][-2:])
+                pbd['block'] = pbn[3].split('+')[0]
+                pbd['orientation'] = pbn[3].split('+')[-1]
+                pb_lod.append(pbd)
+            pb_df = pd.DataFrame(pb_lod)
+
+
+            config['blocks'] = {
+                'subj_id': pb_df.subj.unique()[0],
+                'blocks': ' '.join(pb_df.block.unique()),
+            }
+
+            # try:
+            mot_path = make_motion_plot(subj_out_dir, subject_label)
+            config['motion_report'] = read_report_snippet(mot_path)
+            # except FileNotFoundError:
+            #    pass
+
+            warn_list = ['3dDeconvolve.err',
+                         'out.pre_ss_warn.txt',
+                         'out.cormat_warn.txt']
+
+            warns = {}
+            for wf in warn_list:
+                wf_path = os.path.join(subj_out_dir, wf)
+                try:
+                    if os.path.getsize(wf_path) > 0:
+                        with open(wf_path, 'r') as h:
+                            warns['wf'] = h.readlines()
+                except FileNotFoundError:
+                    pass
+            if len(warns) > 0:
+                config['warnings'] = warns
+
+            if not os.path.exists(subj_qc_dir):
+                os.mkdir(subj_qc_dir)
+            if not os.path.exists(subj_qc_img_dir):
+                os.mkdir(subj_qc_img_dir)
+            if not os.path.exists(reports_dir):
+                os.mkdir(reports_dir)
+
+            try:
+
+                anat_path = os.path.join(subj_out_dir, 'anat_final.%s+tlrc.HEAD'%subject_label)
+                anat_exts = np.array([float(ss) for ss in subprocess.check_output(["3dinfo", "-extent", anat_path]).decode().split('\t')])
+                anat_lrext = np.abs(anat_exts[0]) + np.abs(anat_exts[1])
+                anat_mont_dim = np.floor(np.sqrt(anat_lrext))
+                print("#######\n mont_dim = %f \n#########"%anat_mont_dim)
+                run(make_montage(os.path.join(subj_qc_img_dir, 'anatomical_montage'),
+                                 ulay=anat_path,
+                                 montx=anat_mont_dim, monty=anat_mont_dim), shell=True)
+                
+
+                func_path = pb_df.loc[pb_df['block'] == 'volreg', 'path'].values[0] + '[0]'
+                func_rext = float(subprocess.check_output(["3dinfo", "-Rextent", func_path]))
+                func_lext = float(subprocess.check_output(["3dinfo", "-Lextent", func_path]))
+                func_lrext = np.abs(func_lext) + np.abs(func_rext)
+                func_mont_dim = np.floor(np.sqrt(func_lrext))
+
+                run(make_montage(os.path.join(subj_qc_img_dir, 'functional_montage'),
+                                 ulay=anat_path,
+                                 olay=func_path, montx=anat_mont_dim, monty=anat_mont_dim,
+                                 cbar='gray_scale', opacity=9), shell=True)
+
+                with open(os.path.join(subj_qc_img_dir, 'anatomical_montage.sag.jpg'), 'rb') as h:
+                    anat_bs = base64.b64encode(h.read()).decode()
+                with open(os.path.join(subj_qc_img_dir, 'functional_montage.sag.jpg'), 'rb') as h:
+                    func_bs = base64.b64encode(h.read()).decode()
+
+                config['volreg_report_anat'] = anat_bs
+                config['volreg_report_func'] = func_bs
+                config['anat_ap_ext'] = np.abs(anat_exts[2]) + np.abs(anat_exts[3]) + 1
+                config['anat_is_ext'] = np.abs(anat_exts[4]) + np.abs(anat_exts[5]) + 1
+                print("#######\n anat_ap_ext = %f \n#########"%config['anat_ap_ext'])
+            except FileNotFoundError:
+                pass
+
+            tpl = IndividualTemplate()
+            tpl.generate_conf(config, os.path.join(reports_dir, 'sub-%s_individual.html'%subject_label))
+
+            with open(os.path.join(subj_qc_dir, 'individual.json'), 'w') as h:
+                json.dump(config, h)
+
+    elif args.analysis_level == 'group':
+        with open(os.path.join(subj_qc_dir, 'individual.json'), 'r') as h:
+            all_configs.append(json.load(h))
+
+if args.analysis_level == 'group':
+    if not os.path.exists(reports_dir):
+        os.mkdir(reports_dir)
+    tpl = GroupTemplate()
+    #print(all_configs)
+    tpl.generate_conf({'configs':all_configs}, os.path.join(reports_dir, 'group.html'))
